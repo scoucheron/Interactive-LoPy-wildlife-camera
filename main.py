cdb09f59b5e7cd56545fb2764d699b5e7562a8f5
Sverre Coucheron <sco007@post.uit.no>
2017-03-16 14:02:06 +0100

    An attempt on json

--- main.py
+++ main.py
@@ -1,10 +1,128 @@
-from network import LoRa
-import socket
-import binascii
-import time
-import pycom
+#from network import LoRa
+#import socket
+#import binascii
+import datetime
+import random
+import json
+#import pycom
 
-""" Joins the LoRaWAN and blinks a bit """"
+''' Measures the temperature and humidity '''
+def temperature_and_humidity():
+    temp = random.randint(0, 1000)
+    hum = random.randint(0, 1000)
+    return temp, hum
+
+
+''' Measures how much battery is left '''
+def battery():
+    battery_level = random.randint(0, 1000)
+    return battery_level
+
+
+''' Measures the remaining space '''
+def remaining_space():
+    storage_left = random.randint(0, 1000)
+    return storage_left
+
+
+''' Measures the acceleration (if there is one) '''
+def acceleration():
+    acceleration_result = random.randint(0, 1000)
+
+    if acceleration_result < 3:
+        return True
+    else:
+        return False
+
+
+''' Measures the light level in lumen '''
+def light_level():
+    lumen = random.randint(0, 1000)
+
+    if lumen < 3:
+        return True
+    else:
+        return False
+
+
+''' Detects motion through a PIR-sensor '''
+def movement_detection():
+    movement = random.randint(0, 1000)
+
+    if movement < 300:
+        return True
+    else:
+        return False
+
+
+def take_picture():
+    picture = "0014192.jpg"
+    return picture
+
+
+''' Calls on all the sensors and saves the data in JSON on the SD-card '''
+def save_data_json(day):
+    temp_hum = temperature_and_humidity()
+    pic = take_picture()
+
+    day['date'] = ({ 'temp': temp_hum[0],
+                     'hum': temp_hum[1],
+                     'pic': pic })
+
+    with open ('data.txt', 'a') as outfile:
+        json.dump(day, outfile, indent=2)
+
+def main():
+    day = {}
+    day['date'] = []
+
+    while True:
+        movement = movement_detection()
+        if movement:
+            save_data_json(day)
+
+        battery_level = battery()
+        space_left = remaining_space()
+        acceleration_result = acceleration()
+
+        if(battery_level < 3):
+            print('WE ARE OUT OF BATTERY')
+
+        if(space_left < 3):
+            print('WE ARE OUT OF STORAGE')
+
+        if(acceleration_result):
+            print("THE CAMERA HAS FALLEN DOWN. PLS SEND HELP")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+''' Joins the LoRaWAN and blinks a bit '''
 def join_network():
     pycom.heartbeat(False)
     pycom.rgbled(0x000000)
@@ -47,65 +165,17 @@ def join_network():
 
     print ("After while, count is: ",  count)
 
-""" Measures the temperature and humidity """
-def temperature_and_humidity():
-    temp = 10
-    hum = 10
-    return tmp_hum
-
-""" Measures how much battery is left """
-def battery():
-    battery_level = 98
-    return battery_level
-
-""" Measures the remaining space """
-def remaining_space():
-    storage_left = 50
-    return storage_left
-
-""" Measures the acceleration (if there is one) """
-def acceleration():
-    acceleration_result = True
-
-    if acceleration_result:
-        return True
-    else:
-        return False
-
-""" Measures the light level in lumen """
-def light_level():
-    lumen = 100
-
-    if lumen < 100:
-        return True
-    else:
-        return False
-
-""" Calls on all the sensors and saves the data in JSON on the SD-card """
-def save_data_json():
-    temp_hum = temperature_and_humidity()
-    light_lumen = light_level()
-    acceleration_result = acceleration()
-
-    """ Want to send this to the user """
-    battery_level = battery()
-    space_left = remaining_space ()
-
-""" Detects motion through a PIR-sensor """
-def movement_detection():
-    movement = True
-
-    if movement:
-        return True
-    else:
-        return False
-
-""" Sends the relevnt data back to the user """
+''' Sends the relevnt data back to the user '''
 def send_data():
     print("Create LoRaWAN socket")
     # create a raw LoRa socket
     s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
     s.setblocking(False)
+
+    ''' Want to send this to the user if the leves are critical'''
+    battery_level = battery()
+    space_left = remaining_space()
+    acceleration_result = acceleration()
 
     count = 0
     while True:
@@ -126,7 +196,3 @@ def send_data():
         data = s.recv(64)
         print("Received Data:",  data)
         time.sleep(30)
-
-if __name__ == "__main__":
-    join_network()
-    send_data()
